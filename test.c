598d5584fe23c61c693fe992067d5d0408c3d52f
Josephstar23 <omovieakpesiri@gmail.com>
2024-03-07 03:39:33 -0800

    Testing

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/* main - trying to push to github
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	printf("Always check your codes"\n);
+
+	return (0);
+}
